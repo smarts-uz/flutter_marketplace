c502665d14d2a3a117d2267191777fd0654a2016
Pakhlavon <Pahlavon-9596@mail.ru>
2021-01-15 14:37:15 +0500

    change sound

--- android/app/src/main/kotlin/com/example/flutter_marketplace/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_marketplace/MainActivity.kt
@@ -3,7 +3,4 @@ package com.example.flutter_marketplace
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        GeneratedPluginRegistrant.registerWith(flutterEngine);
-    }
 }
