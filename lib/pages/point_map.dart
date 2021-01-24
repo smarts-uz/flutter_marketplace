15c1c33f36d37d3ef841e573c77b62e81acbb56e
abrorbobomurodov1998 <abrorbobomurodov1998@gmail.com>
2021-01-25 02:18:29 +0500

    update bottomsheetDialog

--- lib/pages/point_map.dart
+++ lib/pages/point_map.dart
@@ -8,15 +8,33 @@ class PointMap extends StatefulWidget {
 class _PointMapState extends State<PointMap> {
 
   @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          _settingModalBottomSheet(context);
-        },
-        child: Icon(Icons.add),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            child: Icon(Icons.north, color: Colors.blue),
+            backgroundColor: Colors.white,
+            onPressed: (){
+              _settingModalBottomSheet(context);
+            },
+          ),
+          SizedBox(width: 10),
+          FloatingActionButton(
+            child: Icon(Icons.my_location_rounded, color: Colors.black),
+            backgroundColor: Colors.white,
+          ),
+        ],
       ),
+
+      // bottomSheet: _settingModalBottomSheet(context),
       appBar: AppBar(
         backgroundColor: Colors.white,
         leading: IconButton(
@@ -55,99 +73,94 @@ class _PointMapState extends State<PointMap> {
     );
   }
 
-  void _settingModalBottomSheet(BuildContext context) {
-    showModalBottomSheet(context: context, builder: (BuildContext bc){
-      return DefaultTabController(
-        length: 2,
-        child: Column(
-          children: [
-            Align(
-              alignment: Alignment.centerRight,
-              child: Container(
-                color: Colors.white,
-                child: Column(
-                  children: [
-                    Align(
-                      alignment: Alignment.centerRight,
-                      child: Container(
-                        child: IconButton(icon: Icon(Icons.close), onPressed: (){
-                          Navigator.pop(context);
-                        },),
-                      ),
-                    ),
-                    TabBar(
-                      // These are the widgets to put in each tab in the tab bar.
-                      tabs: [
-                       Text('ASHOP PREMIUM', style: TextStyle(color: Colors.black),),
-                       Text('БЕЗ ASHOP PREMIUM', style: TextStyle(color: Colors.black),),
-                      ]
-                    ),
-             SizedBox(
-               height: 200,
+  void _settingModalBottomSheet(BuildContext context) async{
+    await Future.delayed(Duration.zero);
+    showModalBottomSheet(
+        isScrollControlled: true,
+        context: context,
+        builder: (BuildContext bc){
+      return DraggableScrollableSheet(
+        expand: false,
+        builder: (context, scrollController){
+          return DefaultTabController(
+            length: 2,
+            child: Column(
+              children: [
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: Container(
+                    child: IconButton(icon: Icon(Icons.close), onPressed: (){
+                      Navigator.pop(context);
+                    },),
+                  ),
+                ),
+                TabBar(
+                  // These are the widgets to put in each tab in the tab bar.
+                  tabs: [
+                   Text('ASHOP PREMIUM', style: TextStyle(color: Colors.black),),
+                   Text('БЕЗ ASHOP PREMIUM', style: TextStyle(color: Colors.black),),
+                  ]
+                ),
+             Expanded(
                child: TabBarView(
-                 children: [
-                  tabitem(),
-                 tabitem()
-                 ],
+               children: [
+                tabitem(scrollController),
+                tabitem(scrollController)
+               ],
                ),
              ),
-                  ],
-                ),
-
-              ),
+              ],
             ),
-          ],
-        ),
+          );
+        },
       );
     }
     );
   }
 
-  Padding tabitem() {
+  Padding tabitem(ScrollController c) {
+
     return Padding(
                   padding: const EdgeInsets.only(left: 30, right: 30),
-                  child: Container(
-                    child: SingleChildScrollView(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+                  child: ListView(
+                    controller: c,
+         //             crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(height: 20),
+                     Row(
+                      children: [
+                      Icon(Icons.home_outlined, color: Colors.black),
+                      SizedBox(width: 20),
+                      Text("Пункты выдачи и постаматы"),
+                      ],
+                      ),
+                      SizedBox(height: 15),
+                      Text("Для заказов с предоплатой и постоплатой", style: TextStyle(color: Color.fromRGBO(157,160,160, 1))),
+                     SizedBox(height: 15),
+                      Row(
                         children: [
-                          SizedBox(height: 20),
-                         Row(
-                          children: [
-                          Icon(Icons.home_outlined, color: Colors.black),
-                          SizedBox(width: 20),
-                          Text("Пункты выдачи и постаматы"),
-                          ],
-                          ),
-                          SizedBox(height: 15),
-                          Text("Для заказов с предоплатой и постоплатой", style: TextStyle(color: Color.fromRGBO(157,160,160, 1))),
-                         SizedBox(height: 15),
-                          Row(
-                            children: [
-                              Text('●'),
-                              SizedBox(width: 10),
-                              Text("стоимость доставки 0 P")
-                            ],
-                          ),
-                          SizedBox(height: 40),
-                          Padding(
-                            padding: const EdgeInsets.only(left: 8.0, right: 8.0),
-                            child: Row(
-                              children: [
-                                Icon(Icons.archive),
-                                Text("Почта Узбекистана")
-                              ],
-                            ),
-                          ),
-                          SizedBox(height: 30),
-                          Text("Для предоплаченных заказов", style: TextStyle(color: Color.fromRGBO(157,160,160, 1))),
-                          SizedBox(height: 10),
-                          Text('● минимальная сумма заказа от 999 р-\nстоимость доставки рассчитывается по тарифам Почты Тошкент'),
-                          SizedBox(height: 10),
-                          Text('● доставка Почтой Ташкент доступна только по предоплате'),
+                          Text('●'),
+                          SizedBox(width: 10),
+                          Text("стоимость доставки 0 P")
                         ],
                       ),
-                    ),
+                      SizedBox(height: 40),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                        child: Row(
+                          children: [
+                            Icon(Icons.archive),
+                            Text("Почта Узбекистана")
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 30),
+                      Text("Для предоплаченных заказов", style: TextStyle(color: Color.fromRGBO(157,160,160, 1))),
+                      SizedBox(height: 10),
+                      Text('● минимальная сумма заказа от 999 р-\nстоимость доставки рассчитывается по тарифам Почты Тошкент'),
+                      SizedBox(height: 10),
+                      Text('● доставка Почтой Ташкент доступна только по предоплате'),
+                    ],
                   ),
                 );
   }
